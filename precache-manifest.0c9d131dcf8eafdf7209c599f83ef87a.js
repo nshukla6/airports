d8b231ce6c050814f6e012ff4fa0b5fbc320b93b
nitin <nshukla6@gmail.com>
2020-08-02 21:47:47 +0530

    Updates

---
+++ precache-manifest.0c9d131dcf8eafdf7209c599f83ef87a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29f48206640df4fa39a804a52bfefbc5",
+    "url": "/airports/index.html"
+  },
+  {
+    "revision": "e5b81091fcfd4e12e10d",
+    "url": "/airports/static/css/2.8e9cee04.chunk.css"
+  },
+  {
+    "revision": "6d9d3c9abc4ef454fcec",
+    "url": "/airports/static/css/main.33f945b2.chunk.css"
+  },
+  {
+    "revision": "e5b81091fcfd4e12e10d",
+    "url": "/airports/static/js/2.e6b70a26.chunk.js"
+  },
+  {
+    "revision": "78f25236c37fc20bcef8a9a310ca6cda",
+    "url": "/airports/static/js/2.e6b70a26.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d9d3c9abc4ef454fcec",
+    "url": "/airports/static/js/main.f0c6f600.chunk.js"
+  },
+  {
+    "revision": "dd50eaf81d175bf92c87",
+    "url": "/airports/static/js/runtime-main.c7835522.js"
+  }
+]);
